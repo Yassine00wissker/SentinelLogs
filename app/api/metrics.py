139fa474ccccd9aa5682c64a855d99adf58c08d7
unknown <yassineyahyaoui40@gmail.com>
2026-01-20 21:12:01 +0100

    Refactor metrics using Pandas

--- app/api/metrics.py
+++ app/api/metrics.py
@@ -2,44 +2,51 @@ from fastapi import APIRouter, Depends
 from app.dependencies import get_db
 from app.models import Log
 from sqlalchemy.orm import Session
-from collections import Counter
+import pandas as pd
 
 router = APIRouter()
 
-def total_logs(db: Session):
-    return db.query(Log).count()
+def get_logs_df(db:Session) -> pd.DataFrame:
+    logs = db.query(Log.timestamp, Log.level, Log.service).all()
 
-def logs_by_level(db: Session):
-    logs =  db.query(Log.level).all()
-    return dict(Counter(log.level for log in logs))
-
-def logs_by_service(db: Session):
-    logs =  db.query(Log.service).all()
-    return dict(Counter(log.service for log in logs))
-
-def error_rate(db:Session):
-    total = db.query(Log).count()
-    if total == 0:
-        return 0
-    errors = db.query(Log).filter(Log.level == "ERROR").count()
-    return errors / total
-
-def logs_per_minute(db:Session):
-    logs = db.query(Log.timestamp).all()
-    result = {}
-
-    for log in logs:
-        minute = log.timestamp.strftime("%Y-%m-%d %H:%M")
-        result[minute] = result.get(minute, 0) + 1
+    if not logs:
+        return pd.DataFrame(columns=["timestamp","level","service"])
     
-    return result
+    df = pd.DataFrame(logs, columns=["timestamp","level","service"])
+    df["timestamp"] = pd.to_datetime(df["timestamp"])
+    return df
 
 @router.get("/metrics")
 def get_metrics(db:Session = Depends(get_db)):
+    df = get_logs_df(db)
+
+    total_logs = len(df)
+
+    logs_by_level = (
+        df["level"].value_counts().to_dict()
+    )
+
+    logs_by_service = (
+        df["service"].value_counts().to_dict()
+    )
+
+    error_rate = (
+        df["level"].eq("ERROR").mean()
+        if total_logs > 0 else 0
+    )
+
+    logs_per_minute = (
+        df.set_index("timestamp")
+        .resample("1min")
+        .size()
+        .rename(lambda x: x.strftime("%Y-%m-%d %H:%M"))
+        .to_dict()
+    )
+
     return {
-        "total_logs": total_logs(db),
-        "logs_by_level": logs_by_level(db),
-        "logs_by_service": logs_by_service(db),
-        "error_rate": error_rate(db),
-        "logs_per_minute": logs_per_minute(db)
+        "total_logs": total_logs,
+        "logs_by_level": logs_by_level,
+        "logs_by_service": logs_by_service,
+        "error_rate": error_rate,
+        "logs_per_minute": logs_per_minute
     }
